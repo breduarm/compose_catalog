d4e8bb39693dd353568be5bae76d93d469f77cf5
breduarm <bryan.e.armijos@gmail.com>
2024-04-18 00:10:28 -0500

    Add MyBox and MyColumn compose components

--- app/src/main/java/com/beam/composecatalog/MainActivity.kt
+++ app/src/main/java/com/beam/composecatalog/MainActivity.kt
@@ -3,13 +3,24 @@ package com.beam.composecatalog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.beam.composecatalog.ui.theme.Compose_catalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +33,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android, this my first catalog of components in Jetpack Compose")
+                    MyColumn()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "Albert Einstein", modifier = Modifier.background(color = Color.Blue))
+        Text(text = "Marie Curie", modifier = Modifier.background(color = Color.Yellow))
+        Text(text = "Isaac Newton", modifier = Modifier.background(color = Color.Red))
+        Text(text = "Charles Darwin", modifier = Modifier.background(color = Color.Green))
+        Text(text = "Galileo Galilei", modifier = Modifier.background(color = Color.Magenta))
+    }
+}
+
+@Composable
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .background(Color.Cyan)
+                .verticalScroll(
+                    rememberScrollState()
+                )
+        )
     }
 }
 
@@ -39,8 +80,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     Compose_catalogTheme {
-        Greeting("Android")
+        MyColumn()
     }
 }
