5a9c40c87ea5c29a2e4c3f6b3598cb270213d50b
breduarm <bryan.e.armijos@gmail.com>
2024-04-19 09:43:25 -0500

    Add MyState

--- app/src/main/java/com/beam/composecatalog/MainActivity.kt
+++ app/src/main/java/com/beam/composecatalog/MainActivity.kt
@@ -17,10 +17,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,10 +43,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyConstraintLayout()
+                    MyState()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyState() {
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter += 1 }) {
+            Text(text = "Don't pulse")
+        }
+        Text(text = "Pushed $counter times")
     }
 }
 
@@ -198,6 +218,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun DefaultPreview() {
     Compose_catalogTheme {
-        MyConstraintLayout()
+        MyState()
     }
 }
