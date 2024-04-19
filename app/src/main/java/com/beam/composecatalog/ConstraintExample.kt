01a12d386bcc33e9c4685c6a2886090bf624bf9d
breduarm <bryan.e.armijos@gmail.com>
2024-04-19 01:59:26 -0500

    Solve Exercise number 2

--- app/src/main/java/com/beam/composecatalog/ConstraintExample.kt
+++ app/src/main/java/com/beam/composecatalog/ConstraintExample.kt
@@ -10,31 +10,96 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.beam.composecatalog.ui.theme.Compose_catalogTheme
 
-@Preview(showBackground = true)
+@Composable
+fun Exercise2() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (boxRed, boxGreen, boxBlue, boxYellow, boxCyan) = createRefs()
+
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                top.linkTo(boxRed.bottom)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Blue)
+            .constrainAs(boxBlue) {
+                top.linkTo(boxRed.bottom)
+                start.linkTo(boxRed.end)
+            })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Cyan)
+            .constrainAs(boxCyan) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
+    }
+}
+
 @Composable
 fun MyConstraintLayout() {
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
         val (boxRed, boxGreen, boxYellow, boxCyan) = createRefs()
 
-        Box(modifier = Modifier.size(100.dp).background(Color.Red).constrainAs(boxRed){
-            top.linkTo(parent.top)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            bottom.linkTo(parent.bottom)
-        })
-        Box(modifier = Modifier.size(100.dp).background(Color.Green).constrainAs(boxGreen){
-            bottom.linkTo(boxYellow.top)
-            start.linkTo(boxYellow.end)
-            end.linkTo(boxCyan.start)
-        })
-        Box(modifier = Modifier.size(100.dp).background(Color.Yellow).constrainAs(boxYellow){
-            bottom.linkTo(boxRed.top)
-            end.linkTo(boxRed.start)
-        })
-        Box(modifier = Modifier.size(100.dp).background(Color.Cyan).constrainAs(boxCyan){
-            bottom.linkTo(boxRed.top)
-            start.linkTo(boxRed.end)
-        })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                bottom.linkTo(boxYellow.top)
+                start.linkTo(boxYellow.end)
+                end.linkTo(boxCyan.start)
+            })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Cyan)
+            .constrainAs(boxCyan) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConstrainExamplePreview() {
+    Compose_catalogTheme {
+        Exercise2()
     }
 }
