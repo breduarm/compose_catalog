8449e417744411903ec7366a02c88d214dcb896d
breduarm <bryan.e.armijos@gmail.com>
2024-04-20 02:16:08 -0500

    Add MyCheckboxWithText

--- app/src/main/java/com/beam/composecatalog/MySelectionComponents.kt
+++ app/src/main/java/com/beam/composecatalog/MySelectionComponents.kt
@@ -1,17 +1,35 @@
 package com.beam.composecatalog
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.beam.composecatalog.ui.theme.Compose_catalogTheme
+
+@Composable
+fun MyCheckboxWithText() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        MyCheckbox()
+        Text(text = "This is a checkbox")
+    }
+}
 
 @Composable
 fun MyCheckbox() {
@@ -39,6 +57,6 @@ fun MySwitch() {
 @Composable
 fun SelectComponentDefaultPreview() {
     Compose_catalogTheme {
-        MyCheckbox()
+        MyCheckboxWithText()
     }
 }
