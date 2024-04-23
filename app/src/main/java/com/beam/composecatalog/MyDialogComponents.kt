e336a754d4b54068c06a524e3c3fd833318cb1be
breduarm <bryan.e.armijos@gmail.com>
2024-04-22 23:30:50 -0500

    Add MyCustomDialog

--- app/src/main/java/com/beam/composecatalog/MyDialogComponents.kt
+++ app/src/main/java/com/beam/composecatalog/MyDialogComponents.kt
@@ -1,8 +1,11 @@
 package com.beam.composecatalog
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -15,9 +18,34 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.beam.composecatalog.ui.theme.Compose_catalogTheme
+
+@Composable
+fun MyCustomDialog(show: Boolean, onDismiss: () -> Unit) {
+    if (show) {
+        Dialog(
+            onDismissRequest = onDismiss,
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(24.dp)
+            ) {
+                Text(text = "This i a example of a custom dialog")
+            }
+        }
+    }
+}
 
 @Composable
 fun MyTestDialog() {
@@ -26,11 +54,15 @@ fun MyTestDialog() {
         Button(onClick = { show = !show }) {
             Text(text = "Display AlertDialog")
         }
-        MyAlertDialog(
-            show = show,
-            onConfirm = { Log.i("beam", "Alert dialog confirmed") },
-            onDismiss = { show = false }
-        )
+        if (false) {
+            MyAlertDialog(
+                show = show,
+                onConfirm = { Log.i("beam", "Alert dialog confirmed") },
+                onDismiss = { show = false }
+            )
+        } else {
+            MyCustomDialog(show = show, onDismiss = { show = false })
+        }
     }
 }
 
