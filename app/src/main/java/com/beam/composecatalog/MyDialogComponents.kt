b44bb048bd95a37d71621a68f91ccecc79799a9a
breduarm <bryan.e.armijos@gmail.com>
2024-04-22 23:17:45 -0500

    Add MyDialog and MyTestDialog

--- app/src/main/java/com/beam/composecatalog/MyDialogComponents.kt
+++ app/src/main/java/com/beam/composecatalog/MyDialogComponents.kt
@@ -1,13 +1,59 @@
 package com.beam.composecatalog
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.beam.composecatalog.ui.theme.Compose_catalogTheme
+
+@Composable
+fun MyTestDialog() {
+    var show by rememberSaveable { mutableStateOf(false) }
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(onClick = { show = !show }) {
+            Text(text = "Display AlertDialog")
+        }
+        MyDialog(
+            show = show,
+            onConfirm = { Log.i("beam", "Alert dialog confirmed") },
+            onDismiss = { show = false }
+        )
+    }
+}
+
+@Composable
+fun MyDialog(show: Boolean, onConfirm: () -> Unit, onDismiss: () -> Unit) {
+    if (show) {
+        AlertDialog(
+            title = { Text(text = "Title") },
+            text = { Text(text = "This is the content of the alert") },
+            confirmButton = {
+                TextButton(onClick = onConfirm) {
+                    Text(text = "Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = onDismiss) {
+                    Text(text = "Dismiss")
+                }
+            },
+            onDismissRequest = onDismiss
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -18,7 +64,7 @@ fun DialogComponentsDefaultPreview() {
                 .fillMaxSize()
                 .padding(24.dp)
         ) {
-            MyRangeSlider()
+            MyTestDialog()
         }
     }
 }
