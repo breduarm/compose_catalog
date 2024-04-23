938d3bb90c83a27ecc6acacdec0593a6a8932b56
breduarm <bryan.e.armijos@gmail.com>
2024-04-22 23:49:52 -0500

    Add AlertType enum and MyCustomDialogGmailAccountChange

--- app/src/main/java/com/beam/composecatalog/MyDialogComponents.kt
+++ app/src/main/java/com/beam/composecatalog/MyDialogComponents.kt
@@ -19,11 +19,41 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.beam.composecatalog.ui.theme.Compose_catalogTheme
+
+@Composable
+fun MyCustomDialogGmailAccountChange(show: Boolean, onDismiss: () -> Unit) {
+    if (show) {
+        Dialog(
+            onDismissRequest = onDismiss,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(24.dp)
+            ) {
+                MyTitleDialog("Set backup account")
+            }
+        }
+    }
+}
+
+@Composable
+fun MyTitleDialog(text: String) {
+    Text(
+        text = text,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(bottom = 12.dp)
+    )
+}
 
 @Composable
 fun MyCustomDialog(show: Boolean, onDismiss: () -> Unit) {
@@ -47,6 +77,10 @@ fun MyCustomDialog(show: Boolean, onDismiss: () -> Unit) {
     }
 }
 
+enum class AlertType {
+    ALERT_DIALOG, CUSTOM_DIALOG, GMAIL_ACCOUNT_CHANGE_DIALOG
+}
+
 @Composable
 fun MyTestDialog() {
     var show by rememberSaveable { mutableStateOf(false) }
@@ -54,14 +88,21 @@ fun MyTestDialog() {
         Button(onClick = { show = !show }) {
             Text(text = "Display AlertDialog")
         }
-        if (false) {
-            MyAlertDialog(
-                show = show,
-                onConfirm = { Log.i("beam", "Alert dialog confirmed") },
-                onDismiss = { show = false }
-            )
-        } else {
-            MyCustomDialog(show = show, onDismiss = { show = false })
+        val alertType = AlertType.GMAIL_ACCOUNT_CHANGE_DIALOG
+        when (alertType) {
+            AlertType.ALERT_DIALOG -> {
+                MyAlertDialog(
+                    show = show,
+                    onConfirm = { Log.i("beam", "Alert dialog confirmed") },
+                    onDismiss = { show = false }
+                )
+            }
+            AlertType.CUSTOM_DIALOG -> {
+                MyCustomDialog(show = show, onDismiss = { show = false })
+            }
+            AlertType.GMAIL_ACCOUNT_CHANGE_DIALOG -> {
+                MyCustomDialogGmailAccountChange(show = show, onDismiss = { show = false })
+            }
         }
     }
 }
