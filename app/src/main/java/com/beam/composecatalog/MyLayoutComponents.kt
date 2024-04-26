7d0d6ffef074b6e86acc05a62b68887187afbd22
breduarm <bryan.e.armijos@gmail.com>
2024-04-26 17:38:29 -0500

    Move some components from MainActivity to MyLayoutComponents

---
+++ app/src/main/java/com/beam/composecatalog/MyLayoutComponents.kt
@@ -0,0 +1,201 @@
+package com.beam.composecatalog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.beam.composecatalog.ui.theme.Compose_catalogTheme
+
+@Composable
+fun MyState() {
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter += 1 }) {
+            Text(text = "Don't pulse")
+        }
+        Text(text = "Pushed $counter times")
+    }
+}
+
+@Composable
+fun MySpacer(size: Int) {
+    Spacer(modifier = Modifier.height(size.dp))
+}
+
+@Composable
+fun Exercise1() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Cyan), contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Header", color = Color.Black)
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.White)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Red), contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Left side", color = Color.White)
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Green), contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Right side", color = Color.White)
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Magenta), contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(text = "Footer")
+        }
+    }
+}
+
+@Composable
+fun MyComplexComponent() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(Color.Yellow)
+        )
+        MySpacer(30)
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.LightGray)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Gray), contentAlignment = Alignment.Center
+            ) {
+                Text(text = "This a text", color = Color.White)
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .background(Color.DarkGray)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(Color.Cyan)
+        ) {
+
+        }
+    }
+}
+
+@Composable
+fun MyRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState()),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(text = "J. Robert Oppenheimer", modifier = Modifier.weight(1f))
+        Text(text = "Enrico Fermi", modifier = Modifier.weight(1f))
+        Text(text = "Niels Bohr", modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun MyColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "Albert Einstein", modifier = Modifier.background(color = Color.Blue))
+        Text(text = "Marie Curie", modifier = Modifier.background(color = Color.Yellow))
+        Text(text = "Isaac Newton", modifier = Modifier.background(color = Color.Red))
+        Text(text = "Charles Darwin", modifier = Modifier.background(color = Color.Green))
+        Text(text = "Galileo Galilei", modifier = Modifier.background(color = Color.Magenta))
+    }
+}
+
+@Composable
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .background(Color.Cyan)
+                .verticalScroll(
+                    rememberScrollState()
+                )
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LayoutComponentsDefaultPreview() {
+    Compose_catalogTheme {
+        MyState()
+    }
+}
